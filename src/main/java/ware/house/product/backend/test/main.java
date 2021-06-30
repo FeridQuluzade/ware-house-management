a1b3a8c7ea8ff966407d5caf6cf80ab0de2ebbef
Ferid07 <feridquluzade2002@gmail.com>
2021-06-30 07:41:08 +0400

    WareHouse-9: change Controller structure for WareHouseProduct model

--- src/main/java/ware/house/product/backend/test/main.java
+++ src/main/java/ware/house/product/backend/test/main.java
@@ -1,28 +1,48 @@
 package ware.house.product.backend.test;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ware.house.product.backend.config.AutoFill;
+import ware.house.product.backend.dao.WareHouseRepository;
+import ware.house.product.backend.dto.WareHouseUpdateDto;
+import ware.house.product.backend.service.*;
+
+import java.time.LocalDateTime;
+import java.util.Scanner;
 
 
 public class main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+//        WareHouseUpdateDto wareHouseUpdateDto= new WareHouseUpdateDto();
+//        wareHouseUpdateDto.setName("Floor");
+//        wareHouseUpdateDto.setCount(5);
+//        wareHouseUpdateDto.setBuyRate(10);
+//        wareHouseUpdateDto.setSellRate(12);
+//        wareHouseUpdateDto.setId(5);
+//        wareHouseUpdateDto.setUpdatedDate(LocalDateTime.now());
+//        DbService dbService= new PostgreDbServiceImpl();
+//       WareHouseRepository wareHouseRepository= new WareHouseRepository(dbService);
+//       ModelMapper modelMapper= new ModelMapper();
+//        WareHouseService wareHouseService= new WareHouseServiceImpl(wareHouseRepository,modelMapper);
+//        wareHouseService.update(wareHouseUpdateDto);
 
-//        Scanner scanner = new Scanner(System.in);
-//
-//        DbService dbService = new PostgreDbServiceImpl();
-//
-//        ModelMapper modelMapper = new ModelMapper();
-//
-//        WareHouseRepository wareHouseRepository = new WareHouseRepository(dbService);
-//
-//        WareHouseService wareHouseService = new WareHouseServiceImpl(wareHouseRepository, modelMapper);
-//
-//        AutoFill autoFill = new AutoFill(wareHouseService, scanner);
-//
-//        MenuService menuService = new MenuServiceImpl(autoFill, scanner);
-//
-//        menuService.menuItem();
+
+        DbService dbService = new PostgreDbServiceImpl();
+
+        ModelMapper modelMapper = new ModelMapper();
+
+        WareHouseRepository wareHouseRepository = new WareHouseRepository(dbService);
+
+        WareHouseService wareHouseService = new WareHouseServiceImpl(wareHouseRepository, modelMapper);
+
+        AutoFill autoFill = new AutoFill(wareHouseService, scanner);
+
+        MenuService menuService = new MenuServiceImpl(autoFill, scanner);
+
+        menuService.menuItem();
 
 
     }
